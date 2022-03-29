5f2b813d8ac5094e0f6b37ef867ac218f1908d94
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2022-03-29 11:40:46 -0700

    inline styles.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,7 +23,7 @@
     <body class="antialiased">
 
 
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0" style="text-align: center;">
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
 

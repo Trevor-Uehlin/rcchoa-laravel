103f68ef25f4983fac2cef90b9397b30ce4a11d2
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2022-03-29 10:35:39 -0700

    isAdministrator function for user class.

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function isAdministrator() {
+
+        return $this->user_type == "administrator";
+    }
 }

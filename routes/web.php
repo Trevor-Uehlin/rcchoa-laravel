1ad1994b4344e489bcf68521a82e2ddd866a33c3
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2022-03-29 11:33:08 -0700

    Admin changes.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,16 +18,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::group(["middleware" => "auth"], function(){
 
-Route::get("/home", function(){ 
-    return view("home");
-})->name("home");
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+    
+    Route::get("/home", function(){ 
+        return view("home");
+    })->name("home");
+    
+    Route::get("/documents", function(){ 
+        return view("documents");
+    })->name("documents");
 
-Route::get("/documents", function(){ 
-    return view("documents");
-})->name("documents");
+});
+
+
 
 require __DIR__.'/auth.php';

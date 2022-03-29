1617ef211b10dc5438999665c8dea38c5e97e312
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2022-03-29 11:47:43 -0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::group(["middleware" => "web"], function(){
 
-Route::get('/', function () {
-    return view('welcome');
+    Route::get('/', function () {
+        return view('welcome');
+    });
 });
+
 
 Route::group(["middleware" => "auth"], function(){
 
